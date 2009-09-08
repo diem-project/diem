c6aa9ce851fad8e807518ff4b8798d6d1470621a
Thibault Duplessis <thibault.duplessis@gmail.com>
2009-09-08 10:04:22 +0000

    [Diem] updated settings form

--- dmAdminPlugin/modules/dmUserLog/actions/actions.class.php
+++ dmAdminPlugin/modules/dmUserLog/actions/actions.class.php
@@ -1,43 +1,31 @@
 <?php
 
-class dmUserLogActions extends dmAdminBaseActions
+class dmConfigPanelActions extends dmAdminBaseActions
 {
-	
-	public function executeClear(dmWebRequest $request)
-	{
-    $log = new dmUserLog;
-		$log->clear();
-		$this->getUser()->logInfo(dm::getI18n()->__('User log cleared'));
-		return $this->redirect('dmUserLog/index');
-	}
-  
-	public function executeRefresh(dmWebRequest $request)
-	{
-		$log = new dmUserLog;
 
-		$responseHash = md5(dmArray::first($log->getEntries(1))->toJson());
-		
-		if ($responseHash == $request->getParameter('hash'))
-		{
-			return $this->renderText('-');
-		}
-		
-    $viewClass = 'dmUserLogView'.dmString::camelize($request->getParameter('view'));
-		
-		$view = new $viewClass($log, $this->getUser()->getCulture());
-		
-		return $this->renderText(
-		  $view->renderBody($request->getParameter('max', 20)).
-		  '__DM_SPLIT__'.
-  	  $responseHash
-		);
-	}
-	
   public function executeIndex(dmWebRequest $request)
   {
-  	$log = new dmUserLog;
-    $this->view = new dmUserLogView($log, $this->getUser()->getCulture());
-    $this->filesize = $log->getSize();
+    $this->form = new dmConfigForm;
+
+    $this->settings = dmDb::table('DmSetting')->fetchGrouped();
+
+    $this->groups = array_keys($this->settings);
+
+    foreach($this->settings as $group => $settings)
+    {
+      foreach($settings as $setting)
+      {
+        if($this->getUser()->can($setting->get('credentials')))
+        {
+          $this->form->addSetting($setting);
+
+          if(!in_array($setting->get('group_name'), $this->groups))
+          {
+            $this->groups[] = $setting->get('group_name');
+          }
+        }
+      }
+    }
   }
-  
+
 }
--- dmAdminPlugin/modules/dmUserLog/templates/indexSuccess.php
+++ dmAdminPlugin/modules/dmUserLog/templates/indexSuccess.php
@@ -1,17 +1,43 @@
 <?php
-use_stylesheet('core.browsers');
-use_stylesheet('admin.userLog');
+use_stylesheet('lib.ui-tabs');
+use_stylesheet('admin.configPanel');
+use_javascript('lib.ui-tabs');
+use_javascript('admin.configPanel');
 
-echo £o('div.dm_box.big.user_log');
+echo £o('div.dm_box.big.sitemap');
 
-echo £('div.title',
-  £link('dmUserLog/clear')->text(__('Clear')).
-  £('h1', __('User log').sprintf(' ( %s )', dmOs::humanizeSize($filesize)))
-);
+echo £('h1.title', __('Edit configuration'));
 
-echo £o('div.dm_box_inner');
+echo £o('div.dm_box_inner.dm_config_panel');
 
-echo $view->render(200);
+echo £o('ul');
+foreach($groups as $group)
+{
+  echo £('li', sprintf('<a href="#%s">%s</a>', dmString::slugify($group), __(dmString::humanize($group))));
+}
+echo £c('ul');
+
+echo $form->open('.dm_form.list');
+
+foreach($settings as $group => $groupSettings)
+{
+  echo £o('div#'.dmString::slugify($group));
+  
+  echo £('h2', __(dmString::humanize($group)));
+  
+  echo £o('ul.dm_setting_group.clearfix');
+  foreach($groupSettings as $setting)
+  {
+    echo $form[$setting->get('name')]->renderRow();
+  }
+  echo £c('ul');
+  
+  echo £c('div');
+}
+
+echo $form->renderSubmitTag();
+
+echo '</form>';
 
 echo £c('div');
 
