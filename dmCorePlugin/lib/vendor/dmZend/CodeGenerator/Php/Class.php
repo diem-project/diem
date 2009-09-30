cdd13bb5a4d82c26f64abd0c240c26eb6d99a2ab
Thibault Duplessis <thibault.duplessis@gmail.com>
2009-09-30 12:54:05 +0000

    [Diem] updated Zend Framework components to 1.9.3PL1

---
+++ dmCorePlugin/lib/vendor/dmZend/CodeGenerator/Php/Class.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once 'dmZend/CodeGenerator/Php/Method.php';
+
+class dmZendCodeGeneratorPhpClass extends Zend_CodeGenerator_Php_Class
+{
+  public static function fromReflection(Zend_Reflection_Class $reflectionClass)
+  {
+    $class = new self();
+
+    $class->setSourceContent($class->getSourceContent());
+    $class->setSourceDirty(false);
+
+    if ($reflectionClass->getDocComment() != '') {
+      $class->setDocblock(Zend_CodeGenerator_Php_Docblock::fromReflection($reflectionClass->getDocblock()));
+    }
+
+    $class->setAbstract($reflectionClass->isAbstract());
+    $class->setName($reflectionClass->getName());
+
+    if ($parentClass = $reflectionClass->getParentClass()) {
+      $class->setExtendedClass($parentClass->getName());
+      $interfaces = array_diff($parentClass->getInterfaces(), $reflectionClass->getInterfaces());
+    } else {
+      $interfaces = $reflectionClass->getInterfaces();
+    }
+
+    $class->setImplementedInterfaces($interfaces);
+
+    $properties = array();
+    foreach ($reflectionClass->getProperties() as $reflectionProperty) {
+      if ($reflectionProperty->getDeclaringClass()->getName() == $class->getName()) {
+        $properties[] = Zend_CodeGenerator_Php_Property::fromReflection($reflectionProperty);
+      }
+    }
+    $class->setProperties($properties);
+
+    $methods = array();
+    foreach ($reflectionClass->getMethods() as $reflectionMethod) {
+      if ($reflectionMethod->getDeclaringClass()->getName() == $class->getName()) {
+        $methods[] = dmZendCodeGeneratorPhpMethod::fromReflection($reflectionMethod);
+      }
+    }
+    $class->setMethods($methods);
+
+    return $class;
+  }
+}
---
+++ dmCorePlugin/lib/vendor/dmZend/CodeGenerator/Php/Method.php
@@ -0,0 +1,96 @@
+<?php
+
+class dmZendCodeGeneratorPhpMethod extends Zend_CodeGenerator_Php_Method
+{
+
+  public static function fromReflection(Zend_Reflection_Method $reflectionMethod)
+  {
+    $method = new self();
+
+    $method->setSourceContent($reflectionMethod->getContents(false));
+    $method->setSourceDirty(false);
+
+    if ($reflectionMethod->getDocComment() != '') {
+      $method->setDocblock(Zend_CodeGenerator_Php_Docblock::fromReflection($reflectionMethod->getDocblock()));
+    }
+
+    $method->setFinal($reflectionMethod->isFinal());
+
+    if ($reflectionMethod->isPrivate()) {
+      $method->setVisibility(self::VISIBILITY_PRIVATE);
+    } elseif ($reflectionMethod->isProtected()) {
+      $method->setVisibility(self::VISIBILITY_PROTECTED);
+    } else {
+      $method->setVisibility(self::VISIBILITY_PUBLIC);
+    }
+
+    $method->setStatic($reflectionMethod->isStatic());
+
+    $method->setName($reflectionMethod->getName());
+
+    foreach ($reflectionMethod->getParameters() as $reflectionParameter) {
+      $method->setParameter(Zend_CodeGenerator_Php_Parameter::fromReflection($reflectionParameter));
+    }
+
+    $body = $reflectionMethod->getBody();
+    $body2 = str_replace("\n\n", "\n", $body);
+    $body2 = preg_replace("|^\n\s{4}|muU", "\n", $body2);
+    $body2 = preg_replace("|^\s{4}|muU", "", $body2);
+//    $body2 = str_replace(' ', '.', $body2);
+//dmDebug::kill($body, "\n".$body2);
+    $method->setBody($body2);
+
+    return $method;
+  }
+
+  /**
+   * generate()
+   *
+   * @return string
+   */
+  public function generate()
+  {
+    $output = '';
+
+    $indent = $this->getIndentation();
+
+    if (($docblock = $this->getDocblock()) !== null) {
+      $docblock->setIndentation($indent);
+      $output .= $docblock->generate();
+    }
+
+    $output .= $indent;
+
+    if ($this->isAbstract()) {
+      $output .= 'abstract ';
+    } else {
+      $output .= (($this->isFinal()) ? 'final ' : '');
+    }
+
+    $output .= $this->getVisibility()
+    . (($this->isStatic()) ? ' static' : '')
+    . ' function ' . $this->getName() . '(';
+
+    $parameters = $this->getParameters();
+    if (!empty($parameters)) {
+      foreach ($parameters as $parameter) {
+        $parameterOuput[] = $parameter->generate();
+      }
+
+      $output .= implode(', ', $parameterOuput);
+    }
+
+    $output .= ')' . self::LINE_FEED . $indent . '{' . self::LINE_FEED;
+
+    if ($this->_body) {
+      $output .= str_repeat($indent, 2)
+      .  str_replace(self::LINE_FEED, self::LINE_FEED . $indent . $indent, trim($this->_body))
+      .  self::LINE_FEED;
+    }
+
+    $output .= $indent . '}' . self::LINE_FEED;
+
+    return $output;
+  }
+
+}
