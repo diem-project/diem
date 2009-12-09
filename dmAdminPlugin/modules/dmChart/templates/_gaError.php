76c743c49df9cee4a3f291edc2ee0124b2530917
Anselme <nomail@nomail.com>
2009-12-09 14:17:28 +0000

    Traduction

--- dmAdminPlugin/modules/dmChart/templates/_gaError.php
+++ dmAdminPlugin/modules/dmChart/templates/_gaError.php
@@ -4,5 +4,5 @@ echo £('div.s16.s16_info', __('This chart is not available.'));
 
 if ($sf_user->can('google_analytics'))
 {
-  echo £link('@dm_google_analytics')->text('To enable it, provide a google analytics access');
+  echo £link('@dm_google_analytics')->text(__('To enable it, provide a google analytics access'));
 }
