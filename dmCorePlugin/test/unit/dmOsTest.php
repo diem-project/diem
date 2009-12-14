4446d1ecec3126611c8c57295b993821b88cbc60
Thibault Duplessis <thibault.duplessis@gmail.com>
2009-12-14 16:35:05 +0000

    [Diem] fixed and improved dmOsTest

--- dmCorePlugin/test/unit/dmOsTest.php
+++ dmCorePlugin/test/unit/dmOsTest.php
@@ -4,24 +4,34 @@ require_once(dirname(__FILE__).'/helper/dmUnitTestHelper.php');
 $helper = new dmUnitTestHelper();
 $helper->boot();
 
-$t = new lime_test(4);
+$t = new lime_test(5);
 
-$t->ok(
+$t->is(
   dmOs::join("home"),
-  "/home"
+  "/home",
+  'dmOs::join /home'
 );
 
-$t->ok(
+$t->is(
   dmOs::join(sfConfig::get("sf_root_dir"), "/cache/"),
-  sfConfig::get("sf_root_dir")."/cache"
+  sfConfig::get("sf_root_dir")."/cache",
+  'dmOs::join sfConfig::get("sf_root_dir"), "/cache/"'
 );
 
-$t->ok(
+$t->is(
   dmOs::join("home", "user/dir", "file.ext"),
-  "/home/user/dir/file.ext"
+  "/home/user/dir/file.ext",
+  'dmOs::join "home", "user/dir", "file.ext"'
 );
 
-$t->ok(
+$t->is(
   dmOs::join("//home///user//dir/file.ext///"),
-  "/home/user/dir/file.ext"
+  "/home/user/dir/file.ext",
+  'dmOs::join "//home///user//dir/file.ext///"'
+);
+
+$t->is(
+  dmOs::sanitizeDirName('A strange DIR name é & / \ ( $'),
+  $expected = 'A-strange-DIR-name-e',
+  'dmOs::sanitizeDirName() : '.$expected
 );
