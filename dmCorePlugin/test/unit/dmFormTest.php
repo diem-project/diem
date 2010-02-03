d57e789bf09bfcb40ab08daf775f4068490a8bd6
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-02-03 16:50:18 +0000

    [Diem] - added unit tests for form rendering fluent interface

--- dmCorePlugin/test/unit/dmFormTest.php
+++ dmCorePlugin/test/unit/dmFormTest.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__).'/helper/dmUnitTestHelper.php');
 $helper = new dmUnitTestHelper();
 $helper->boot('front');
 
-$t = new lime_test(14);
+$t = new lime_test(17);
 
 $forms = $helper->get('form_manager');
 
@@ -28,6 +28,8 @@ $t->isa_ok($form, 'DmUserForm', 'Created a DmUserForm');
 
 $form->setName('first_dmUser_form');
 
+$form->getWidgetSchema()->setHelp('username', 'username help');
+
 $t->is($form->getName(), 'first_dmUser_form', 'Changed form name to first_dmUser_form');
 
 unset($form);
@@ -44,8 +46,20 @@ $got = (string)$form['username']->label('label_test');
 $expected = '<label class="label" for="first_dmUser_form_username">label_test</label>';
 $t->is($got, $expected, $got);
 
+$got = (string)$form['username']->help();
+$expected = '<div class="dm_help_wrap">username help</div>';
+$t->is($got, $expected, $got);
+
+$got = (string)$form['username']->error();
+$expected = '';
+$t->is($got, $expected, $got);
+
 $got = (string)$form['username']->field();
 $expected = '<input type="text" name="first_dmUser_form[username]" class="required" id="first_dmUser_form_username" />';
+$t->is($got, $expected, $got);
+
+$got = (string)$form['username']->label('label_test')->field()->help()->error();
+$expected = '<label class="label" for="first_dmUser_form_username">label_test</label><input type="text" name="first_dmUser_form[username]" class="required" id="first_dmUser_form_username" /><div class="dm_help_wrap">username help</div>';
 $t->is($got, $expected, $got);
 
 $t->is(substr_count((string)$form, 'first_dmUser_form[_csrf_token]'), 1, 'CRSF protection outputed once');
