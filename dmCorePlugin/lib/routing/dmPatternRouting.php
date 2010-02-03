31a2e5e9951df04d46f453e0e017cc014bb61073
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-02-03 18:23:30 +0000

    [Diem] - disabled routing cache for jQuery random requests to avoid APC overhead

--- dmCorePlugin/lib/routing/dmPatternRouting.php
+++ dmCorePlugin/lib/routing/dmPatternRouting.php
@@ -17,4 +17,30 @@ class dmPatternRouting extends sfPatternRouting
     parent::initialize($dispatcher, $cache, $options);
   }
 
+  /*
+   * Disable cache when the request contain a "_" paremeter
+   * This parameter is set randomly by jQuery to avoid browser cache
+   * Cache it quickly leads to thousands cache entries
+   */
+  public function findRoute($url)
+  {
+    if($this->cache && (strpos($this->options['context']['request_uri'], '?_=') || strpos($this->options['context']['request_uri'], '&_=')))
+    {
+      // remember the cache and disable it
+      $cache = $this->cache;
+      $this->cache = null;
+
+      // get the result without cache
+      $result = parent::findRoute($url);
+
+      // restore the cache
+      $this->cache = $cache;
+
+      return $result;
+    }
+    else
+    {
+      return parent::findRoute($url);
+    }
+  }
 }
