72d5c870d57ecbe0ac41bb4871e20e9cff6168c1
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-02-13 04:31:21 +0100

    added edit link in admin page tree

--- dmAdminPlugin/modules/dmInterface/templates/_pageBar.php
+++ dmAdminPlugin/modules/dmInterface/templates/_pageBar.php
@@ -2,7 +2,7 @@
 
 echo _open('div#dm_page_bar');
 
-  echo _tag('p.title', __('Site tree'));
+  echo _tag('p.title', __('Site tree')._link('dmPage/tree')->text(__('Edit'))->set('.fright.mr10.s16.s16_sort'));
 
   echo '<div id="dm_page_tree" class="dm_tree"></div>';
 
