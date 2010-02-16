8cbbc8e7ab5a245a1fb0c598817dc93f6e91781b
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-02-16 16:13:03 +0100

    fixed admin page edit tree stylesheet dir

--- dmAdminPlugin/modules/dmPage/lib/dmAdminFullPageTreeView.php
+++ dmAdminPlugin/modules/dmPage/lib/dmAdminFullPageTreeView.php
@@ -2,4 +2,19 @@
 
 class dmAdminFullPageTreeView extends dmAdminPageTreeView
 {
+
+  protected function renderOpenLi(array $page)
+  {
+    if($page[1] === 'show')
+    {
+      $type = 'auto';
+    }
+    else
+    {
+      $type = $this->lastLevel === false ? 'root' : 'manual';
+    }
+    
+    return '<li id="dmp'.$page[0].'" rel="'.$type.'">';
+  }
+  
 }
--- dmAdminPlugin/web/js/dmAdminPageTree.js
+++ dmAdminPlugin/web/js/dmAdminPageTree.js
@@ -6,54 +6,42 @@
 
     $tree.tree({
       ui: {
-        theme_name: 'dm_page_tree'
+        theme_path: $.dm.ctrl.options.dm_core_asset_root + 'lib/dmTree/',
+        theme_name: 'page-edit',
+        animation	: 200
       },
       types: {
-        "default" : {
-          clickable	: false,
-          renameable	: true,
-          deletable	: true,
-          creatable	: true,
-          draggable	: true,
-          max_children	: -1,
-          max_depth	: -1,
-          valid_children	: "all",
-
-          icon : {
-            image: $.dm.ctrl.options.dm_core_asset_root+'images/16/sprite.png',
-            position: '0 -848px;'
-          }
+        'default': {
+          icon: { image:  $.dm.ctrl.options.dm_core_asset_root + 'images/16/sprite.png'},
+          clickable: true,
+          renameable: true,
+          deletable: true,
+          creatable: false,
+          draggable: true,
+          max_children: -1,
+          max_depth: -1,
+          valid_children: "all"
         },
-        "auto" : {
-          clickable	: false,
-          renameable	: true,
-          deletable	: true,
-          creatable	: true,
-          draggable	: true,
-          max_children	: -1,
-          max_depth	: -1,
-          valid_children	: "all",
-
-          icon : {
-            image: $.dm.ctrl.options.dm_core_asset_root+'images/16/sprite.png',
-            position: '0 -848px;'
-          }
+        'manual': {
+          icon: { position: '0 -864px;'}
         },
-        "manual" : {
-          clickable	: false,
-          renameable	: true,
-          deletable	: true,
-          creatable	: true,
-          draggable	: true,
-          max_children	: -1,
-          max_depth	: -1,
-          valid_children	: "all",
-
-          icon : {
-            image: $.dm.ctrl.options.dm_core_asset_root+'images/16/sprite.png',
-            position: '0 -848px;'
-          }
+        'auto': {
+          icon: { position: '0 -848px;'},
+          renameable: false,
+          deletable: false,
+          creatable: false,
+          draggable: false
+        },
+        'root': {
+          icon: { position: '0 -864px;'},
+          renameable: true,
+          deletable: false,
+          creatable: false,
+          draggable: false
         }
+      },
+      plugins: {
+        
       }
     });
 
