500b52f24eb413bc141540b5d9534aa94054fa6b
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-02-16 02:30:12 +0100

    fixed translations

--- dmAdminPlugin/data/generator/dmAdminDoctrineModule/dmAdmin/template/templates/_list_th_tabular.php
+++ dmAdminPlugin/data/generator/dmAdminDoctrineModule/dmAdmin/template/templates/_list_th_tabular.php
@@ -4,10 +4,11 @@
 
 <th class="sf_admin_<?php echo strtolower($field->getType()) ?> sf_admin_list_th_<?php echo $name ?>">
 <?php if ($field->isReal()): ?>
+  [?php $translatedLabel = __('<?php echo $field->getConfig('label', '', true) ?>'); ?]
   [?php if ('<?php echo $name ?>' == $sort[0]): ?]
-    [?php echo link_to(__('<?php echo $field->getConfig('label', '', true) ?>'), '@<?php echo $this->getUrlForAction('list') ?>', array('class' => 's16 s16_sort_'.$sort[1], 'query_string' => 'sort=<?php echo $name ?>&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'), 'title' => __('Sort by %field%', array('%field%' => '<?php echo $field->getConfig('label', '', true) ?>')))) ?]
+    [?php echo link_to($translatedLabel, '@<?php echo $this->getUrlForAction('list') ?>', array('class' => 's16 s16_sort_'.$sort[1], 'query_string' => 'sort=<?php echo $name ?>&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'), 'title' => __('Sort by %field%', array('%field%' => $translatedLabel)))) ?]
   [?php else: ?]
-    [?php echo link_to(__('<?php echo $field->getConfig('label', '', true) ?>'), '@<?php echo $this->getUrlForAction('list') ?>', array('class' => 's16 s16_right_little', 'query_string' => 'sort=<?php echo $name ?>&sort_type=asc', 'title' => __('Sort by %field%', array('%field%' => '<?php echo $field->getConfig('label', '', true) ?>')))) ?]
+    [?php echo link_to($translatedLabel, '@<?php echo $this->getUrlForAction('list') ?>', array('class' => 's16 s16_right_little', 'query_string' => 'sort=<?php echo $name ?>&sort_type=asc', 'title' => __('Sort by %field%', array('%field%' => $translatedLabel)))) ?]
   [?php endif; ?]
 <?php else: ?>
   [?php echo __('<?php echo $field->getConfig('label', '', true) ?>') ?]
