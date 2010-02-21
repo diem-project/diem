fe62225c7d99bdda11228ce74cd502d3481b8bcd
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-02-21 16:21:59 +0100

    allowed to call dmDroppableInput on a set of elemets

--- dmCorePlugin/web/js/dmCoreEditPlugins.js
+++ dmCorePlugin/web/js/dmCoreEditPlugins.js
@@ -7,43 +7,46 @@
 
     dmDroppableInput: function(callback)
     {
-      var $input = this;
-      
-      if (!$input.hasClass('ui-droppable'))
+      return this.each(function()
       {
-        if($input.hasClass('page_only'))
-        {
-          accept = '#dm_page_bar li';
-        }
-        else if($input.hasClass('media_only'))
-        {
-          accept = '#dm_media_bar li';
-        }
-        else
-        {
-          accept = '#dm_page_bar li, #dm_media_bar li.file';
-        }
+        var $input = $(this);
 
-        $input.droppable({
-          accept: accept,
-          activeClass: 'droppable_active',
-          hoverClass: 'droppable_hover',
-          //          tolerance:    'touch',
-          drop: function(e, ui)
+        if (!$input.hasClass('ui-droppable'))
+        {
+          if($input.hasClass('page_only'))
           {
-            if (ui.draggable.hasClass('file'))
-            {
-              $input.val('media:' + ui.draggable.attr('id').replace(/dmm/, '') + ' ' + ui.draggable.find('span.name:first').text().replace(/\s/g, ''));
-            }
-            else
-            {
-              $input.val('page:' + ui.draggable.attr('id').replace(/dmp/, '') + ' ' + ui.draggable.find('>a').text());
-            }
-            
-            callback && $.isFunction(callback) && callback.apply($input);
+            accept = '#dm_page_bar li';
           }
-        });
-      }
+          else if($input.hasClass('media_only'))
+          {
+            accept = '#dm_media_bar li';
+          }
+          else
+          {
+            accept = '#dm_page_bar li, #dm_media_bar li.file';
+          }
+
+          $input.droppable({
+            accept: accept,
+            activeClass: 'droppable_active',
+            hoverClass: 'droppable_hover',
+            //          tolerance:    'touch',
+            drop: function(e, ui)
+            {
+              if (ui.draggable.hasClass('file'))
+              {
+                $input.val('media:' + ui.draggable.attr('id').replace(/dmm/, '') + ' ' + ui.draggable.find('span.name:first').text().replace(/\s/g, ''));
+              }
+              else
+              {
+                $input.val('page:' + ui.draggable.attr('id').replace(/dmp/, '') + ' ' + ui.draggable.find('>a').text());
+              }
+
+              callback && $.isFunction(callback) && callback.apply($input);
+            }
+          });
+        }
+      });
     },
   
     maxLength: function(max)
