fbdc12265dab7ee269a62512750294e7414ad9e3
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-02-27 23:15:26 +0100

    changed search widget form name format to make search more pretty

--- dmFrontPlugin/lib/dmWidget/search/form/dmSearchForm.php
+++ dmFrontPlugin/lib/dmWidget/search/form/dmSearchForm.php
@@ -5,7 +5,8 @@ class dmSearchForm extends BaseForm
 
   public function configure()
   {
-    $this->setName('search');
+    $this->setName('search')
+    ->widgetSchema->setNameFormat('%s');
     
     $this->widgetSchema['query'] = new sfWidgetFormInputText();
     
