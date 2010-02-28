ab6d3bbd79a8c6310a35a586a0f0dc326dc6d868
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-02-28 19:26:18 +0100

    fixed translations

--- dmFrontPlugin/lib/dmWidget/search/dmWidgetSearchResultsForm.php
+++ dmFrontPlugin/lib/dmWidget/search/dmWidgetSearchResultsForm.php
@@ -11,7 +11,7 @@ class dmWidgetSearchResultsForm extends dmWidgetPluginForm
      * Max per page
      */
     $this->widgetSchema['maxPerPage']     = new sfWidgetFormInputText(array(
-      'label' => 'Max per page'
+      'label' => 'Per page'
     ), array(
       'size' => 3
     ));
