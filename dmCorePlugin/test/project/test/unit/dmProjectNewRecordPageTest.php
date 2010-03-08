0fd966354c071ca3e4b7389eb40b7021553e7115
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-03-08 22:01:09 +0100

    added unit tests for record page creation

---
+++ dmCorePlugin/test/project/test/unit/dmProjectNewRecordPageTest.php
@@ -0,0 +1,74 @@
+<?php
+
+require_once(realpath(dirname(__FILE__).'/../../..').'/unit/helper/dmUnitTestHelper.php');
+$helper = new dmUnitTestHelper();
+$helper->boot('front');
+
+$t = new lime_test();
+
+$helper->get('page_tree_watcher')->connect();
+
+$nbPage = dmDb::table('DmPage')->count();
+
+$domain = dmDb::table('DmTestDomain')->create(array(
+  'title' => dmString::random(),
+  'is_active' => false
+))->saveGet();
+
+$t->ok($domain->exists(), 'Record has been saved');
+
+$t->ok(!$domain->isActive, 'Record is not active');
+
+$t->is(dmDb::table('DmPage')->count(), $nbPage, 'No new page');
+
+$t->ok($page = $domain->getDmPage(), 'Domain has a page');
+
+$t->is(dmDb::table('DmPage')->count(), $nbPage+1, 'One new page');
+
+$t->is($page->isActive, false, 'The page is not active');
+
+$t->is($page->Record, $domain, 'The page record is the domain');
+
+$domain = dmDb::table('DmTestDomain')->create(array(
+  'title' => dmString::random(),
+  'is_active' => true
+))->saveGet();
+
+$t->ok($domain->exists(), 'Record has been saved');
+
+$t->ok($domain->isActive, 'Record is active');
+
+$t->is(dmDb::table('DmPage')->count(), $nbPage+1, 'No new page');
+
+$t->ok($page = $domain->getDmPage(), 'Domain has a page');
+
+$t->is(dmDb::table('DmPage')->count(), $nbPage+2, 'One new page');
+
+$t->is($page->isActive, true, 'The page is active');
+
+$t->is($page->Record, $domain, 'The page record is the domain');
+
+$categ = dmDb::table('DmTestCateg')->create(array(
+  'name' => dmString::random(),
+  'is_active' => false
+))->saveGet();
+
+$t->is(dmDb::table('DmPage')->count(), $nbPage+2, 'No new page');
+
+$t->ok(!$categ->getDmPage(), 'Categ has a NO page');
+
+$t->is(dmDb::table('DmPage')->count(), $nbPage+2, 'No new page');
+
+$t->comment('link the categ to a domain');
+$categ->Domains->add($domain);
+$categ->save();
+
+$t->is(dmDb::table('DmPage')->count(), $nbPage+2, 'No new page');
+
+$t->ok($page = $categ->getDmPage(), 'Categ has a page');
+
+$t->is(dmDb::table('DmPage')->count(), $nbPage+3, 'One new page');
+
+$t->is($page->isActive, false, 'The page is not active');
+
+$t->is($page->Record, $categ, 'The page record is the categ');
