6b053ef839042ac686056b92c576ba81bd8df9bc
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-03-18 16:31:00 +0100

    changing password in admin now requires to enter the old password - Closes #78

--- dmCorePlugin/plugins/dmUserPlugin/modules/dmUserAdmin/lib/base/BaseDmUserAdminMyAccountForm.php
+++ dmCorePlugin/plugins/dmUserPlugin/modules/dmUserAdmin/lib/base/BaseDmUserAdminMyAccountForm.php
@@ -10,12 +10,24 @@ class BaseDmUserAdminMyAccountForm extends BaseDmUserForm
   {
     parent::setup();
 
-    $this->widgetSchema['password'] = new sfWidgetFormInputPassword(array(), array(
+    $this->widgetSchema['username'] = new sfWidgetFormInputHidden();
+    $this->validatorSchema['username'] = new sfValidatorChoice(array(
+      'choices' => array($this->getObject()->username)
+    ));
+
+    $this->widgetSchema['old_password'] = new sfWidgetFormInputPassword(array(
+      'label' => 'Old password'
+    ));
+    $this->validatorSchema['old_password'] = new sfValidatorString();
+
+    $this->widgetSchema['password'] = new sfWidgetFormInputPassword(array(
+      'label' => 'New password'
+    ), array(
       'autocomplete' => 'off'
     ));
     $this->validatorSchema['password']->setOption('required', false);
     $this->widgetSchema['password_again'] = new sfWidgetFormInputPassword(array(
-      'label' => 'Password (again)'
+      'label' => 'New password (again)'
     ), array(
       'autocomplete' => 'off'
     ));
@@ -25,9 +37,14 @@ class BaseDmUserAdminMyAccountForm extends BaseDmUserForm
 
     $this->changeToEmail('email');
 
-    $this->useFields(array('email', 'password', 'password_again'));
+    $this->useFields(array('email', 'old_password', 'password', 'password_again'));
 
     $this->mergePreValidator(new sfValidatorCallback(array('callback' => array($this, 'currentUserValidator'))));
+
+    $userValidator = new dmValidatorUser();
+    $userValidator->setOption('password_field', 'old_password');
+    $userValidator->setMessage('invalid', 'The password is invalid.');
+    $this->mergePostValidator($userValidator);
 
     $this->mergePostValidator(new sfValidatorSchemaCompare('password', sfValidatorSchemaCompare::EQUAL, 'password_again', array(), array('invalid' => 'The two passwords must be the same.')));
   }
