a35b3b7696f8ef4a8d6d74cf0c35eec74b46d114
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-03-24 19:58:51 +0100

    fixed issue when closing front record fast edit

--- dmAdminPlugin/data/generator/dmAdminDoctrineModule/dmAdmin/template/templates/_form_action_bar_embedded.php
+++ dmAdminPlugin/data/generator/dmAdminDoctrineModule/dmAdmin/template/templates/_form_action_bar_embedded.php
@@ -3,5 +3,5 @@
 [?php
 echo $helper->linkToSave($form->getObject(), <?php echo $this->asPhp($saveParams) ?>);
 ?]
-<li class="ml10"><input type="submit" value="[?php echo __('Close') ?]" onclick="parent.document.getElementById('cboxClose').setAttribute('rel', 'dm_close')" /></li>
+<li class="ml10"><input type="submit" value="[?php echo __('Close') ?]" onclick="parent.document.getElementById('cboxClose').setAttribute('rel', 'dm_close');return false;" /></li>
 </ul>
