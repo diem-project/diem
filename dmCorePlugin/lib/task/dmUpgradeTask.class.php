14d78945a4e9fb5706a0c963515bf73466125163
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-03-28 20:07:01 +0200

    completed multiple areas automatic upgrade

--- dmCorePlugin/lib/task/dmUpgradeTask.class.php
+++ dmCorePlugin/lib/task/dmUpgradeTask.class.php
@@ -50,8 +50,46 @@ class dmUpgradeTask extends dmContextTask
 
   /**
    * Allow multiple layout and page areas
+   * Will upgrade templates in apps/front/modules/dmFront/templates/***Success.php
    */
   protected function upgradeToMultipleAreas()
   {
+    $areaNameChanges = array(
+      'top'     => 'layout.top',
+      'left'    => 'layout.left',
+      'right'   => 'layout.right',
+      'bottom'  => 'layout.bottom',
+      'content' => 'page.content'
+    );
+
+    $templates = array_unique(dmDb::query('DmLayout l')
+    ->select('l.template')
+    ->fetchFlat());
+    
+    foreach($templates as $template)
+    {
+      $file = dmOs::join(sfConfig::get('sf_apps_dir'), 'front/modules/dmFront/templates', $template.'Success.php');
+      if(!$code = @file_get_contents($file))
+      {
+        continue;
+      }
+      $newCode = $code;
+      foreach($areaNameChanges as $old => $new)
+      {
+        $newCode = str_replace("\$helper->renderArea('{$old}'", "\$helper->renderArea('{$new}'", $newCode);
+      }
+      if($newCode != $code)
+      {
+        if(is_writable($file))
+        {
+          $this->logSection('diem', 'Upgrade '.$file);
+          file_put_contents($file, $newCode);
+        }
+        else
+        {
+          $this->logBlock('Can not upgrade '.$file.', unsuficient permissions', 'ERROR');
+        }
+      }
+    }
   }
 }
