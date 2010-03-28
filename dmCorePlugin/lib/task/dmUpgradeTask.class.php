1c86a7dc78081ad6d545d60c1f545c66abcccae5
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-03-28 19:50:43 +0200

    removed old upgrades

--- dmCorePlugin/lib/task/dmUpgradeTask.class.php
+++ dmCorePlugin/lib/task/dmUpgradeTask.class.php
@@ -6,13 +6,8 @@
 class dmUpgradeTask extends dmContextTask
 {
   protected
-  $diemVersions = array(
-    'addGaToken',
-    'clearLogs',
-    'authDmUserModule',
-    'authDmUserAdminModule',
-    'renameLoginPage',
-    'fixBaseFormFilterDoctrineSetup'
+  $changes = array(
+    'multipleAreas'
   );
   
   /**
@@ -38,9 +33,9 @@ class dmUpgradeTask extends dmContextTask
     
     $this->logSection('diem', 'Upgrade '.dmProject::getKey());
     
-    foreach($this->diemVersions as $version)
+    foreach($this->changes as $change)
     {
-      $upgradeMethod = 'upgradeTo'.ucfirst($version);
+      $upgradeMethod = 'upgradeTo'.ucfirst($change);
       
       try
       {
@@ -48,133 +43,15 @@ class dmUpgradeTask extends dmContextTask
       }
       catch(Exception $e)
       {
-        $this->logBlock('Can not upgrade to version '.$version.' : '.$e->getMessage(), 'ERROR');
+        $this->logBlock('Can not upgrade to change '.$change.' : '.$e->getMessage(), 'ERROR');
       }
     }
   }
 
   /**
-   * Add ga_token setting if missing
+   * Allow multiple layout and page areas
    */
-  protected function upgradeToAddGaToken()
+  protected function upgradeToMultipleAreas()
   {
-    if(!dmConfig::has('ga_token'))
-    {
-      $setting = new DmSetting;
-      $setting->set('name', 'ga_token');
-      $setting->fromArray(array(
-        'description' => 'Auth token gor Google Analytics, computed from password',
-        'group_name'  => 'internal',
-        'credentials' => 'google_analytics'
-      ));
-
-      $setting->save();
-    }
-  }
-
-  /**
-   * Clear old school formatted logs
-   */
-  protected function upgradeToClearLogs()
-  {
-    foreach(array('request', 'event') as $logName)
-    {
-      $file = dmOs::normalize(sfConfig::get('sf_data_dir').'/dm/log/'.$logName.'.log');
-
-      if(file_exists($file) && false !== strpos(file_get_contents($file), '{"time":'))
-      {
-        $this->logSection('upgrade', 'Cleared old school formatted log '.$logName);
-        file_put_contents($file, '');
-      }
-    }
-  }
-
-  /**
-   * Fix login and secure module in front settings.yml
-   */
-  protected function upgradeToAuthDmUserModule()
-  {
-    // Front : Replace login and secure module: dmFront -> dmAuth
-    $settingsFile = dmProject::rootify('apps/front/config/settings.yml');
-    $settingsText = file_get_contents($settingsFile);
-    $settings = sfYaml::load($settingsText);
-
-    foreach(array('.settings', '.actions') as $space)
-    {
-      $loginModule  = dmArray::get(dmArray::get($settings['all'], $space, array()), 'login_module');
-      $loginAction  = dmArray::get(dmArray::get($settings['all'], $space, array()), 'login_action');
-      $secureModule = dmArray::get(dmArray::get($settings['all'], $space, array()), 'secure_module');
-
-      if('dmFront' == $loginModule)
-      {
-        $settingsText = preg_replace('/login_module\:(\s*)dmFront/i', 'login_module:$1dmUser', $settingsText);
-        file_put_contents($settingsFile, $settingsText);
-      }
-      if('login' == $loginAction)
-      {
-        $settingsText = preg_replace('/login_action\:(\s*)login/i', 'login_action:$1signin', $settingsText);
-        file_put_contents($settingsFile, $settingsText);
-      }
-      if('dmFront' == $secureModule)
-      {
-        $settingsText = preg_replace('/secure_module\:(\s*)dmFront/i', 'secure_module:$1dmUser', $settingsText);
-        file_put_contents($settingsFile, $settingsText);
-      }
-    }
-  }
-
-  /*
-   * Fix login and secure module in admin settings.yml
-   */
-  protected function upgradeToAuthDmUserAdminModule()
-  {
-    // Admin : Replace login and secure module: dmAuthAdmin -> dmUserAdmin
-    $settingsFile = dmProject::rootify('apps/admin/config/settings.yml');
-    $settingsText = file_get_contents($settingsFile);
-    $settings = sfYaml::load($settingsText);
-
-    foreach(array('.settings', '.actions') as $space)
-    {
-      $loginModule  = dmArray::get(dmArray::get($settings['all'], $space, array()), 'login_module');
-      $secureModule = dmArray::get(dmArray::get($settings['all'], $space, array()), 'secure_module');
-
-      if('dmAuth' == $loginModule || 'dmAuthAdmin' == $loginModule)
-      {
-        $settingsText = preg_replace('/login_module\:(\s*)\w+/i', 'login_module:$1dmUserAdmin', $settingsText);
-        file_put_contents($settingsFile, $settingsText);
-      }
-      if('dmAuth' == $secureModule || 'dmAuthAdmin' == $loginModule)
-      {
-        $settingsText = preg_replace('/secure_module\:(\s*)\w+/i', 'secure_module:$1dmUserAdmin', $settingsText);
-        file_put_contents($settingsFile, $settingsText);
-      }
-    }
-  }
-
-  protected function upgradeToRenameLoginPage()
-  {
-    if ($page = dmDb::table('DmPage')->findOneByModuleAndAction('main', 'login'))
-    {
-      if(!dmDb::table('DmPage')->findOneByModuleAndAction('main', 'signin'))
-      {
-        $page->set('action', 'signin');
-        $page->save();
-      }
-    }
-  }
-
-  protected function upgradeToFixBaseFormFilterDoctrineSetup()
-  {
-  	// base file path
-  	$file = dmOs::normalize(sfConfig::get('sf_lib_dir').'/filter/doctrine/BaseFormFilterDoctrine.class.php');
-
-  	/// get content
-    $content = file_get_contents($file);
-
-    // remove empty setup method
-    $content = preg_replace('#\s+public function setup\(\)\s+\{\s+\}#i', '', $content);
-
-    // push new content
-    file_put_contents($file, $content);
   }
 }
