4b5cee3b93d9fb1fc34a1b51ac83b1f572d02f1d
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-03-31 09:20:39 +0200

    made DmGallery use table identifier definition - Closes #128

--- dmCorePlugin/lib/doctrine/extension/DmGallery/DmGallery.php
+++ dmCorePlugin/lib/doctrine/extension/DmGallery/DmGallery.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Behavior for adding gallery features to your models
  *
@@ -7,18 +8,27 @@
  */
 class Doctrine_DmGallery extends Doctrine_Record_Generator
 {
+
   public function __construct(array $options = array())
   {
     $this->_options = $options;
   }
-
+  
   public function setTableDefinition()
   {
     $this->setColumnOption('id', 'autoincrement', true);
-    
+
     $this->hasColumn('dm_media_id', 'integer', null, array('notnull' => true));
 
-    $this->hasColumn('dm_record_id', 'integer', null, array('notnull' => true));
+    $identifier = $this->_options['table']->getIdentifier();
+    if (is_array($identifier))
+    {
+      throw new dmException('DmGallery works with unique identifier tables only');
+    }
+    $identifierDefinition = $this->_options['table']->getColumnDefinition($identifier);
+    $unsigned = isset($identifierDefinition['unsigned']) ? $identifierDefinition['unsigned'] : false;
+
+    $this->hasColumn('dm_record_id', $identifierDefinition['type'], $identifierDefinition['length'], array('notnull' => true, 'unsigned' => $unsigned));
 
     $this->hasColumn('position', 'integer');
 
@@ -31,35 +41,35 @@ class Doctrine_DmGallery extends Doctrine_Record_Generator
   public function generateClass(array $definition = array())
   {
     $definition['inheritance']['extends'] = 'myDoctrineRecord';
-    
+
     return parent::generateClass($definition);
   }
-
+  
   public function buildRelation()
   {
     $this->_table->bind(array($this->_options['mediaClass'], array(
-      'local'    => 'dm_media_id',
-      'foreign'  => 'id',
-      'onDelete' => 'CASCADE'
-    )), Doctrine_Relation::ONE);
+        'local'    => 'dm_media_id',
+        'foreign'  => 'id',
+        'onDelete' => 'CASCADE'
+      )), Doctrine_Relation::ONE);
 
     $this->_table->bind(array($this->getOption('table')->getComponentName(), array(
-      'local'    => 'dm_record_id',
-      'foreign'  => 'id',
-      'onDelete' => 'CASCADE'
-    )), Doctrine_Relation::ONE);
+        'local'    => 'dm_record_id',
+        'foreign'  => 'id',
+        'onDelete' => 'CASCADE'
+      )), Doctrine_Relation::ONE);
 
     $this->getOption('table')->bind(array($this->_options['mediaClass'] . ' as ' . $this->_options['mediaAlias'], array(
-      'local'    => 'dm_record_id',
-      'foreign'  => 'dm_media_id',
-      'refClass' => $this->_table->getComponentName(),
-      'orderBy'  => 'position ASC'
-    )), Doctrine_Relation::MANY);
-
+        'local'    => 'dm_record_id',
+        'foreign'  => 'dm_media_id',
+        'refClass' => $this->_table->getComponentName(),
+        'orderBy'  => 'position ASC'
+      )), Doctrine_Relation::MANY);
 //      parent::buildRelation();
   }
 
   public function setUp()
   {
+
   }
 }
