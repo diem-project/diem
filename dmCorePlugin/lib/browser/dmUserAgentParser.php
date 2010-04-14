123129b9321ad5436707e85ab7e4d3505acfb527
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-04-15 01:09:04 +0200

    Add user_agent_parser service

---
+++ dmCorePlugin/lib/browser/dmUserAgentParser.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once(sfConfig::get('dm_core_dir').'/lib/vendor/php-user-agent/lib/phpUserAgentStringParser.php');
+
+class dmUserAgentParser extends phpUserAgentStringParser
+{
+  
+}
