7210f47e427df6e58db5672dedf3349f134d1518
ornicar <thibault.duplessis@gmail.com>
2010-04-23 14:56:58 +0200

    dmForm->bindAndValid now only binds the request if the request has the form name parameter

--- dmCorePlugin/lib/form/dmForm.php
+++ dmCorePlugin/lib/form/dmForm.php
@@ -127,6 +127,7 @@ class dmForm extends sfFormSymfony
   
   /**
    * Binds the current form validate it in one step.
+   * Will bind the request only if the request has the form name parameter.
    *
    * @param  array      An array of tainted values to use to bind the form
    * @param  array      An array of uploaded files (in the $_FILES or $_GET format)
@@ -136,7 +137,7 @@ class dmForm extends sfFormSymfony
    */
   public function bindAndValid(sfWebRequest $request)
   {
-    return $this->bindRequest($request)->isValid();
+    return $request->hasParameter($this->name) && $this->bindRequest($request)->isValid();
   }
   
   public function bindRequest(sfWebRequest $request)
