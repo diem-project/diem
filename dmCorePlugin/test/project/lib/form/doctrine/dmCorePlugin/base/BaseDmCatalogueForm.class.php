e2136d94af9375ef163a3b63837a1621ab94e25d
ornicar <thibault.duplessis@gmail.com>
2010-05-03 11:47:47 +0200

    Update test project

---
+++ dmCorePlugin/test/project/lib/form/doctrine/dmCorePlugin/base/BaseDmCatalogueForm.class.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * DmCatalogue form base class.
+ *
+ * @method DmCatalogue getObject() Returns the current form's model object
+ *
+ * @package    retest
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id$
+ */
+abstract class BaseDmCatalogueForm extends BaseFormDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'id'          => new sfWidgetFormInputHidden(),
+      'name'        => new sfWidgetFormInputText(),
+      'source_lang' => new sfWidgetFormInputText(),
+      'target_lang' => new sfWidgetFormInputText(),
+
+    ));
+
+    $this->setValidators(array(
+      'id'          => new sfValidatorDoctrineChoice(array('model' => $this->getModelName(), 'column' => 'id', 'required' => false)),
+      'name'        => new sfValidatorString(array('max_length' => 255)),
+      'source_lang' => new sfValidatorString(array('max_length' => 15)),
+      'target_lang' => new sfValidatorString(array('max_length' => 15)),
+    ));
+
+    $this->widgetSchema->setNameFormat('dm_catalogue[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+    
+    // Unset automatic fields like 'created_at', 'updated_at', 'position'
+    // override this method in your form to keep them
+    parent::unsetAutoFields();
+  }
+
+
+  protected function doBind(array $values)
+  {
+    parent::doBind($values);
+  }
+  
+  public function processValues($values)
+  {
+    $values = parent::processValues($values);
+    return $values;
+  }
+  
+  protected function doUpdateObject($values)
+  {
+    parent::doUpdateObject($values);
+  }
+
+  public function getModelName()
+  {
+    return 'DmCatalogue';
+  }
+
+}
