69eb6da6f6610d8b4573c8444af1a10d8a1b7b66
servs <nbijnens@servs.eu>
2010-06-15 05:42:21 +0800

    Document in DmGallery: Render as link instead of text.

--- dmCorePlugin/lib/view/html/media/tag/dmMediaTagFile.php
+++ dmCorePlugin/lib/view/html/media/tag/dmMediaTagFile.php
@@ -3,28 +3,8 @@
 class DmMediaTagFile extends DmMediaTag {
 
   public function render() {
-    $tag = 
-      '<img class="media" alt="' . dmOs::getFileName($this->getSrc()) . '" src="'
-      .
-      (file_exists(
-        dmOs::join(
-                sfConfig::get('sf_web_dir')
-                . 
-                  '/dmCorePlugin/images/media/'
-                  .
-                  dmOs::getFileExtension($this->getSrc(), false)
-                  . '.png'
-                )
-              ) ? '/dmCorePlugin/images/media/'
-                  .
-                  dmOs::getFileExtension($this->getSrc(), false)
-                  . '.png'
-                 : '/dmCorePlugin/images/media/unknown.png'
-              )
-      .
-      '" />';
-
-    $tag = '<div class="file file_' . dmOs::getFileExtension($this->getSrc(), false) . '" ><div>' . dmOs::getFileName($this->getSrc()) . '</div></div>';
+    $tag = '<div class="file file_' . dmOs::getFileExtension($this->getSrc(), false) . '" >';
+    $tag .= '<a class="name" href="' . $this->getSrc() . '">' . dmOs::getFileName($this->getSrc()) . '</a></div>';
 
     return $tag;
   }
