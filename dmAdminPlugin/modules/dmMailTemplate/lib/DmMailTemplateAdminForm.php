63c2e57545c04d2879d62f7fe6b7ab19286cd3e1
djacquel <github@pygmeeweb.com>
2010-07-09 23:12:16 +0800

    dmMailTemplate now works with dm_i18n_form = 'embed' parameter generator.yml changed thru a symfony dmAdmin:generate --clear=dmMailTemplate

--- dmAdminPlugin/modules/dmMailTemplate/lib/DmMailTemplateAdminForm.php
+++ dmAdminPlugin/modules/dmMailTemplate/lib/DmMailTemplateAdminForm.php
@@ -13,11 +13,21 @@ class DmMailTemplateAdminForm extends BaseDmMailTemplateForm
   {
     parent::configure();
 
-    $this->widgetSchema['subject'] = new sfWidgetFormInputText();
+    if( 'embed' == sfConfig::get( 'dm_i18n_form' ) ){
 
-    $this->widgetSchema['body']->setAttribute('rows', 15);
+      $cultures = sfConfig::get( 'dm_i18n_cultures' );
 
-    $this->widgetSchema['description']->setAttribute('rows', 2);
+      foreach( $cultures as $culture ){
+        $this->widgetSchema[$culture]['subject'] = new sfWidgetFormInputText();
+        $this->widgetSchema[$culture]['body']->setAttribute( 'rows', 15 );
+        $this->widgetSchema[$culture]['description']->setAttribute( 'rows', 2 );
+      }
+    }else{
+
+      $this->widgetSchema['subject'] = new sfWidgetFormInputText();
+      $this->widgetSchema['body']->setAttribute( 'rows', 15 );
+      $this->widgetSchema['description']->setAttribute( 'rows', 2 );
+    }
     
     // Unset automatic fields like 'created_at', 'updated_at', 'created_by', 'updated_by'
     $this->unsetAutoFields();
