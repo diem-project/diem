2eb4ccdc0929f5575bedc3f749ef5db444744398
antitoxic <antitoxic@gmail.com>
2010-08-01 17:09:06 +0300

    Fix: Make i18n admin console page and admin diagrams page (graphviz dependancies)

--- dmAdminPlugin/modules/dmDiagram/templates/indexSuccess.php
+++ dmAdminPlugin/modules/dmDiagram/templates/indexSuccess.php
@@ -13,7 +13,7 @@ if (!empty($mldProjectImage))
 {
   echo _tag('div.dm_box.big.diagram',
     _tag('div.title',
-      _tag('h2', 'Project Database'._link($mldProjectImage)->text(__('Download'))->target('blank'))
+      _tag('h2', __('Project Database')._link($mldProjectImage)->text(__('Download'))->target('blank'))
     ).
     _tag('div.dm_box_inner',
       _tag('div.full_width_image', _media($mldProjectImage)->set('.panview#mld_project'))
@@ -25,7 +25,7 @@ if (!empty($mldUserImage))
 {
   echo _tag('div.dm_box.big.diagram',
     _tag('div.title',
-      _tag('h2', 'Diem User Database'._link($mldUserImage)->text(__('Download'))->target('blank'))
+      _tag('h2', __('Diem User Database')._link($mldUserImage)->text(__('Download'))->target('blank'))
     ).
     _tag('div.dm_box_inner',
       _tag('div.full_width_image', _media($mldUserImage)->set('.panview#mld_user'))
@@ -37,7 +37,7 @@ if (!empty($mldCoreImage))
 {
   echo _tag('div.dm_box.big.diagram',
     _tag('div.title',
-      _tag('h2', 'Diem Core Database'._link($mldCoreImage)->text(__('Download'))->target('blank'))
+      _tag('h2', __('Diem Core Database')._link($mldCoreImage)->text(__('Download'))->target('blank'))
     ).
     _tag('div.dm_box_inner',
       _tag('div.full_width_image', _media($mldCoreImage)->set('.panview#mld_core'))
@@ -51,11 +51,11 @@ foreach($dicImages as $appName => $image)
 
   echo _tag('div.dm_box.big.diagram',
     _tag('div.title',
-    _tag('h2', dmString::camelize($appName).' : Dependency Injection Container'._link($image)->text(__('Download'))->target('blank'))).
+    _tag('h2', dmString::camelize($appName).__(' : Dependency Injection Container')._link($image)->text(__('Download'))->target('blank'))).
     _tag('div.dm_box_inner',
       ($withDispatcherLinks
-      ? _tag('p.s16.s16_info', _link('+/dmDiagram/index?with_dispatcher_links=0')->text('Hide dispatcher dependencies'))
-      : _tag('p.s16.s16_info', 'As nearly all modules have a reference to dispatcher, these dependencies are hidden. '._link('+/dmDiagram/index?with_dispatcher_links=1')->text('Click here to see them'))
+      ? _tag('p.s16.s16_info', _link('+/dmDiagram/index?with_dispatcher_links=0')->text(__('Hide dispatcher dependencies')))
+      : _tag('p.s16.s16_info', __('As nearly all modules have a reference to dispatcher, these dependencies are hidden. ')._link('+/dmDiagram/index?with_dispatcher_links=1')->text(__('Click here to see them')))
       ).
       _tag('div.full_width_image', _media($image)->set('.panview#panview'.$appName))
     )
