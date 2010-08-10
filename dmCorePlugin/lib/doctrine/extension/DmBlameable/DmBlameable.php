a610c2166cc7874995e8eda991d506d5d084f1fa
eXtreme <jacek.jedrzejewski@gmail.com>
2010-08-10 22:44:43 +0800

    Fixed notice in DmBlameable

--- dmCorePlugin/lib/doctrine/extension/DmBlameable/DmBlameable.php
+++ dmCorePlugin/lib/doctrine/extension/DmBlameable/DmBlameable.php
@@ -141,7 +141,8 @@ class Doctrine_Template_DmBlameable extends Doctrine_Template
      */
     public function setUp()
     {
-     
+      $className = $this->_table->getComponentName();
+      
       if( ! $this->_options['relations']['created']['disabled']) {
         $this->hasOne($this->_options['relations']['created']['class'] . ' as ' . $this->_options['relations']['created']['name'], 
           array('local' => $this->_options['columns']['created']['name'],
