ec10305ded5b2c19c1b59abc6aef23f9d351e080
Stéphane Erard <stephane.erard@gmail.com>
2010-12-18 12:37:47 +0100

    Added i18n front prefix url with culture option

--- dmFrontPlugin/lib/view/html/dmFrontPageTreeView.php
+++ dmFrontPlugin/lib/view/html/dmFrontPageTreeView.php
@@ -5,6 +5,10 @@ class dmFrontPageTreeView extends dmPageTreeView
 
   protected function renderPageLink(array $page)
   {
+    if(sfConfig::get('dm_i18n_prefix_url')){
+      $page[6] = $this->culture . (strlen($page[6])>0 ? '/' . $page[6] : '');
+    }
+     
     return '<a href="'.$page[6].'" data-page-id="'.$page[0].'"><ins></ins>'.$page[5].'</a>';
   }
 
