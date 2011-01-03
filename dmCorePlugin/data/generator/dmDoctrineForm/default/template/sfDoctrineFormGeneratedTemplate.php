88217044855f473703b4c4cd630a5356efccb7b2
Stéphane Erard <eurokkom.se.ext@gmail.com>
2011-01-03 18:42:06 +0100

    fixing handling of some usefull things like one to one and one to many widgets for auto generated forms

--- dmCorePlugin/data/generator/dmDoctrineForm/default/template/sfDoctrineFormGeneratedTemplate.php
+++ dmCorePlugin/data/generator/dmDoctrineForm/default/template/sfDoctrineFormGeneratedTemplate.php
@@ -16,31 +16,52 @@ abstract class Base<?php echo $this->modelName ?>Form extends <?php echo $this->
   {
     $this->setWidgets(array(
 <?php foreach($this->getColumnAggregationKeyFields() as $column):?>
-			'<?php echo $column->getFieldName() ?>'<?php echo str_repeat(' ', $this->getColumnNameMaxLength() - strlen($column->getFieldName())) ?> => new sfWidgetFormChoice(array('choices' => <?php echo $this->arrayExport($this->getSubClassesChoices());?>)),
+			'<?php echo $column->getFieldName() ?>'<?php echo str_repeat(' ', $this->getNumberOfSpaces($column->getFieldName())) ?> => new sfWidgetFormChoice(array('choices' => <?php echo $this->arrayExport($this->getSubClassesChoices());?>)),
 <?php endforeach;?>
-<?php foreach ($this->getColumns(true) as $column): ?>
-      '<?php echo $column->getFieldName() ?>'<?php echo str_repeat(' ', $this->getColumnNameMaxLength() - strlen($column->getFieldName())) ?> => new <?php echo $this->getWidgetClassForColumn($column) ?>(<?php echo $this->getWidgetOptionsForColumn($column) ?>),
+
+<?php foreach ($this->getColumns(true, true) as $column): ?>
+      '<?php echo $column->getFieldName() ?>'<?php echo str_repeat(' ', $this->getNumberOfSpaces($column->getFieldName())) ?> => new <?php echo $this->getWidgetClassForColumn($column) ?>(<?php echo $this->getWidgetOptionsForColumn($column) ?>),
 <?php endforeach; ?>
 
 <?php foreach ($this->getManyToManyRelations() as $relation): ?>
   <?php if ('DmMedia' === $relation->getClass()) continue; ?>
-      '<?php echo $this->underscore($relation['alias']) ?>_list'<?php echo str_repeat(' ', $this->getColumnNameMaxLength() - strlen($this->underscore($relation['alias']).'_list')) ?> => new sfWidgetFormDoctrineChoice(array('multiple' => true, 'model' => '<?php echo $relation['table']->getOption('name') ?>', 'expanded' => true)),
+      '<?php echo $this->underscore($relation['alias']) ?>_list'<?php echo str_repeat(' ', $this->getNumberOfSpaces($this->underscore($relation['alias']).'_list')) ?> => new sfWidgetFormDoctrineChoice(array('multiple' => true, 'model' => '<?php echo $relation['table']->getOption('name') ?>', 'expanded' => true)),
 <?php endforeach; ?>
+
+<?php foreach($this->getOneToManyRelations() as $relation):?>
+			'<?php echo $this->underscore($relation['alias']) ?>_list'<?php echo str_repeat(' ', $this->getNumberOfSpaces($this->underscore($relation['alias']).'_list')) ?> => new sfWidgetFormDoctrineChoice(array('multiple' => true, 'model' => '<?php echo $relation['table']->getOption('name') ?>', 'expanded' => true)),
+<?php endforeach;?>
+
+<?php foreach($this->getOneToOneRelations() as $relation):?>
+			'<?php echo $this->underscore($relation['local']) ?>'<?php echo str_repeat(' ', $this->getNumberOfSpaces($this->underscore($relation['local']))) ?> => new sfWidgetFormDoctrineChoice(array('multiple' => false, 'model' => '<?php echo $relation['table']->getOption('name')?>', 'expanded' => false)),
+<?php endforeach;?>
+
     ));
 
     $this->setValidators(array(
 <?php foreach($this->getColumnAggregationKeyFields() as $column):?>
-			'<?php echo $column->getFieldName() ?>'<?php echo str_repeat(' ', $this->getColumnNameMaxLength() - strlen($column->getFieldName())) ?> => new sfValidatorChoice(array('choices' => <?php echo $this->arrayExport($this->getSubClassesChoicesValidator());?>, 'required' => true)),
+			'<?php echo $column->getFieldName() ?>'<?php echo str_repeat(' ', $this->getNumberOfSpaces($column->getFieldName())) ?> => new sfValidatorChoice(array('choices' => <?php echo $this->arrayExport($this->getSubClassesChoicesValidator());?>, 'required' => true)),
 <?php endforeach;?>
 
-<?php foreach ($this->getColumns(true) as $column): ?>
-      '<?php echo $column->getFieldName() ?>'<?php echo str_repeat(' ', $this->getColumnNameMaxLength() - strlen($column->getFieldName())) ?> => new <?php echo $this->getValidatorClassForColumn($column) ?>(<?php echo $this->getValidatorOptionsForColumn($column) ?>),
+<?php foreach ($this->getColumns(true, true) as $column): ?>
+      '<?php echo $column->getFieldName() ?>'<?php echo str_repeat(' ', $this->getNumberOfSpaces($column->getFieldName())) ?> => new <?php echo $this->getValidatorClassForColumn($column) ?>(<?php echo $this->getValidatorOptionsForColumn($column) ?>),
 <?php endforeach; ?>
+
 <?php foreach ($this->getManyToManyRelations() as $relation): ?>
   <?php if ('DmMedia' === $relation->getClass()) continue; ?>
-      '<?php echo $this->underscore($relation['alias']) ?>_list'<?php echo str_repeat(' ', $this->getColumnNameMaxLength() - strlen($this->underscore($relation['alias']).'_list')) ?> => new sfValidatorDoctrineChoice(array('multiple' => true, 'model' => '<?php echo $relation['table']->getOption('name') ?>', 'required' => false)),
+      '<?php echo $this->underscore($relation['alias']) ?>_list'<?php echo str_repeat(' ', $this->getNumberOfSpaces($this->underscore($relation['alias']).'_list')) ?> => new sfValidatorDoctrineChoice(array('multiple' => true, 'model' => '<?php echo $relation['table']->getOption('name') ?>', 'required' => false)),
 <?php endforeach; ?>
+
+<?php foreach($this->getOneToManyRelations() as $relation):?>
+			'<?php echo $this->underscore($relation['alias']) ?>_list'<?php echo str_repeat(' ', $this->getNumberOfSpaces($this->underscore($relation['alias']).'_list')) ?> => new sfValidatorDoctrineChoice(array('multiple' => true, 'model' => '<?php echo $relation['table']->getOption('name') ?>', 'required' => false)),
+<?php endforeach;?>
+
+<?php foreach($this->getOneToOneRelations() as $relation):?>
+			'<?php echo $this->underscore($relation['local']) ?>'<?php echo str_repeat(' ', $this->getNumberOfSpaces($this->underscore($relation['local']))) ?> => new sfValidatorDoctrineChoice(array('multiple' => false, 'model' => '<?php echo $relation['table']->getOption('name')?>', 'required' => true)),
+<?php endforeach;?>
     ));
+    
+
 <?php foreach($this->getMediaRelations() as $mediaRelation): ?>
 
     /*
