87f1aea6f583934360c631e1f134323bce12c4fe
Stéphane Erard <stephane.erard@gmail.com>
2011-02-11 20:31:10 +0100

    deleting unfortunate die()

--- dmCorePlugin/data/skeleton/lib/model/myDoctrineRecord.php
+++ dmCorePlugin/data/skeleton/lib/model/myDoctrineRecord.php
@@ -4,7 +4,6 @@ abstract class myDoctrineRecord extends dmDoctrineRecord
 {
 	public function preSave($event)
 	{
-		die(dirname(__FILE__));
 		parent::preSave($event);
 		$this->notify('pre-save');
 	}
