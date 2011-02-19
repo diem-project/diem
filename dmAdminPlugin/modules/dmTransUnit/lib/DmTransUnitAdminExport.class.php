23759050d4bc473be7dbed530a9afea94e101b0c
Anton Stoychev <antitoxic@gmail.com>
2011-02-19 14:15:02 +0200

    Fix: Mistake in copied comments in newly added file from a previous commit

--- dmAdminPlugin/modules/dmTransUnit/lib/DmTransUnitAdminExport.class.php
+++ dmAdminPlugin/modules/dmTransUnit/lib/DmTransUnitAdminExport.class.php
@@ -1,11 +1,12 @@
 <?php
 
 /**
- * news admin export
+ * dmTransUnit module configuration.
  *
- * @package    dev
- * @subpackage news
- * @author     Your name here
+ * @package    lantenora
+ * @subpackage dmTransUnit
+ * @author     thibault d
+ * @version    SVN: $Id: form.php 12474 2008-10-31 10:41:27Z fabien $
  */
 class DmTransUnitAdminExport extends myDoctrineTableExport
 {
