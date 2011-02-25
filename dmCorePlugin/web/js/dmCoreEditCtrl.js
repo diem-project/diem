6ce54869a6f138085c7efcf3c2892d669ebf24d5
Cristian Baciu <cristian.baciu@softlandro.com>
2011-02-25 15:07:20 +0200

    Edit dialogs should be created above all other graphic elements. I changed zIndex from   100 to 10000000.

--- dmCorePlugin/web/js/dmCoreEditCtrl.js
+++ dmCorePlugin/web/js/dmCoreEditCtrl.js
@@ -28,7 +28,7 @@
     dialog: function(options)
     {
       var opt = $.extend($.ui.dialog.defaults, {
-        zIndex: 100,
+        zIndex: 10000000,
         dragStart: function(e)
         {
           $(e.target).hide().parent().css('opacity', 0.5);
