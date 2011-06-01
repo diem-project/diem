812fcd3141b11088bcabfa967d33edd55cbeb4a4
Stéphane Erard <stephane.erard@gmail.com>
2011-06-01 10:43:02 +0200

    Fix: dmModuleSecurityAbstract add a prefix to cache keys using MD5ed user & application

--- dmCorePlugin/lib/security/abstracts/dmModuleSecurityAbstract.class.php
+++ dmCorePlugin/lib/security/abstracts/dmModuleSecurityAbstract.class.php
@@ -53,4 +53,31 @@ class dmModuleSecurityAbstract extends dmMetaCache
   {
     return $this->context->getConfiguration()->getApplication();
   }
+  
+  public function get($key, $default = null)
+  {
+  	return parent::get($this->getMD5() . $key, $default);
+  }
+  
+  public function set($key, $value, $lifetime = null)
+  {
+  	parent::set($this->getMD5() . $key, $value, $lifetime);
+  	return $this;
+  }
+  
+  public function has($key)
+  {
+  	return parent::has($this->getMD5() . $key);
+  }
+  
+  public function remove($key)
+  {
+  	parent::remove($this->getMD5() . $key);
+  	return $this;
+  }
+  
+  public function getMD5()
+  {
+  	return md5($this->getApplication() . serialize($this->user->toArray(false)));
+  }
 }
