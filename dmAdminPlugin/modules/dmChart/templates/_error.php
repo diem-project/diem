f6c03a3dfec0090d22b3c65891599a0dd2c44921
Anton Stoychev <antitoxic@gmail.com>
2011-10-20 17:03:26 +0100

    Fixes redmine: http://bugs.diem-project.org/issues/54 non-translated string

--- dmAdminPlugin/modules/dmChart/templates/_error.php
+++ dmAdminPlugin/modules/dmChart/templates/_error.php
@@ -2,4 +2,4 @@
 
 echo _tag('div.s16.s16_info', __('This chart is not yet available.'));
 
-echo _tag('p', 'It will activate soon, when your site have some data to analyze');
+echo _tag('p', __('It will activated as soon as your site have some data to be analyzed'));
