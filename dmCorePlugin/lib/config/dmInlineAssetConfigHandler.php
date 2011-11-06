1a7836e9d6d538119c716bdc9921ce3049e784f9
Anton Stoychev <antitoxic@gmail.com>
2011-11-06 19:20:29 +0000

    Fix: Autoload uncompressed JavaScript file in dev environment if such exists

--- dmCorePlugin/lib/config/dmInlineAssetConfigHandler.php
+++ dmCorePlugin/lib/config/dmInlineAssetConfigHandler.php
@@ -17,7 +17,7 @@ class dmInlineAssetConfigHandler extends dmInlineConfigHandler
   protected function getValues($prefix, $category, $keys)
   {
     $values = array();
-    
+
     // loop through all key/value pairs
     foreach ((array)$keys as $key => $value)
     {
@@ -33,7 +33,7 @@ class dmInlineAssetConfigHandler extends dmInlineConfigHandler
 
     return $values;
   }
-  
+
   public static function getPathFromWebDir($type, $asset)
   {
     $package = substr($asset, 0, strpos($asset, '.'));
@@ -82,12 +82,55 @@ class dmInlineAssetConfigHandler extends dmInlineConfigHandler
       default:
         $path = '/'.dmString::str_replace_once('.', '/', $asset);
     }
-    
+
     if(!isset($path))
     {
       throw new dmException("Can not find path for asset $type.$package.$asset");
     }
-    
+
+    //check for source version of the file in `dev` environment
+    if (sfConfig::get('sf_environment')=='dev') {
+      $webPath = sfConfig::get('sf_web_dir');
+      $fsPath = $webPath.$path;
+
+      $fsSourceFilePath = null;
+      $fsDirName = dirname($fsPath);
+      $fsFilename = basename($fsPath);
+
+      if (strpos($fsFilename, '.min.')) {
+        $possibleFsSourceFilePath = $fsDirName.DIRECTORY_SEPARATOR.str_replace('.min.','.',$fsFilename);
+        if (file_exists($possibleFsSourceFilePath)) {
+          $fsSourceFilePath = $possibleFsSourceFilePath;
+        }
+      }
+      if (is_null($fsSourceFilePath)) {
+        $fsPathDirectories = explode(DIRECTORY_SEPARATOR, $fsDirName);
+        $fsFilename = str_replace('.min.','.',$fsFilename);
+        foreach ( $fsPathDirectories as $fsDirectory ) {
+          if (in_array($fsDirectory, array('min','minified','compressed','minimized'))) {
+            foreach (array('source','unminified','uncompressed','unminimized') as $possibleSourceDirName) {
+              $search = DIRECTORY_SEPARATOR.$fsDirectory.DIRECTORY_SEPARATOR;
+              $replace = DIRECTORY_SEPARATOR.$possibleSourceDirName.DIRECTORY_SEPARATOR;
+              $possibleFsSourceFilePath = str_replace($search, $replace, $fsDirName.DIRECTORY_SEPARATOR).$fsFilename;
+              if (file_exists($possibleFsSourceFilePath)) {
+                $fsSourceFilePath = $possibleFsSourceFilePath;
+                break;
+              }
+            }
+            //break after a directory with minified files is found
+            break;
+          }
+        }
+        unset( $fsDirectory );
+      }
+      if (!is_null($fsSourceFilePath)) {
+        $fs = dmContext::getInstance()->getFilesystem();
+        $path = DIRECTORY_SEPARATOR.$fs->getRelativeDir($webPath.DIRECTORY_SEPARATOR, $fsSourceFilePath);
+      }
+    }
+
+
+
     return $path;
   }
 }
