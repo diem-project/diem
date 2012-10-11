3c248bb4069d030e3b95af9a9aa67bbf1ff02aac
Erik Van Kelst <erik@magicmanage.be>
2012-10-11 10:39:43 +0200

    Added update GAPI account_data_url (Thx @stephaneerard)

--- dmCorePlugin/lib/vendor/gapi/gapi.class.php
+++ dmCorePlugin/lib/vendor/gapi/gapi.class.php
@@ -29,7 +29,7 @@ class gapi
   const http_interface = 'auto'; //'auto': autodetect, 'curl' or 'fopen'
   
   const client_login_url = 'https://www.google.com/accounts/ClientLogin';
-  const account_data_url = 'https://www.google.com/analytics/feeds/accounts/default';
+  const account_data_url = 'https://www.googleapis.com/analytics/v2.4/management/accounts/~all/webproperties/~all/profiles';
   const report_data_url = 'https://www.google.com/analytics/feeds/data';
   const interface_name = 'GAPI-1.3';
   const dev_mode = false;
