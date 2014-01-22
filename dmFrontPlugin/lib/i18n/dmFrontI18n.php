0951c122b791d8ce623255a6b5d3d7763a348b57
Erik Van Kelst <erik@mancloud.eu>
2014-01-22 17:37:04 +0100

    Bugfix default 'messages' value for I18n catalogue in translation helper

--- dmFrontPlugin/lib/i18n/dmFrontI18n.php
+++ dmFrontPlugin/lib/i18n/dmFrontI18n.php
@@ -8,6 +8,14 @@ class dmFrontI18n extends dmI18n
    */
   public function __($string, $args = array(), $catalogue = 'messages')
   {
+    /**
+     * If null is passed as $catalogue, the default value 'messages' is not set! Why?
+     * Solution: -> force set to 'messages' if no value (null, 0, '', false, ..)
+     */
+    if (!$catalogue) {
+      $catalogue = 'messages';
+    }
+    
     $result = $this->__orFalse($string, $args, $catalogue);
 
     if (false === $result && $catalogue !== 'dm')
